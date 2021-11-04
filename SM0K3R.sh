bd0270d36dba074102b1f0bbfb3b9495a0344b0e
Mr-R0Y4L-SM0K3R <91561900+Mr-R0Y4L-SM0K3R@users.noreply.github.com>
2021-11-04 23:12:24 +0530

    Add files via upload

---
+++ SM0K3R.sh
@@ -0,0 +1,51 @@
+echo -e "\e[92mSMOK3R ( press ENTER TO GO ) "
+
+read varbanner
+
+
+cyan='\e[0;36m'
+lightgreen='\e[1;32m'
+red='\e[1;31m'
+yellow='\e[1;33m'
+clear 
+apt install figlet -y
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+sudo apt-get update
+clear
+apt-get install python
+ clear
+apt-get install git 
+clear
+apt-get install  tor
+
+apt-get install python2
+
+apt install python3-pip
+clea
+ apt-get install ruby
+clear
+sudo apt-get install nginx
+clear 
+apt-get install fish
+clear
+sudo apt-get install openjdk-8-jre
+clear
+apt-get install -y nodejs
+clear
+apt-get install nmap 
+echo -e '\e[91m       
+
+         ──▐─▌──▐─▌──
+         ─▐▌─▐▌▐▌─▐▌─
+         ─█▄▀▄██▄▀▄█─
+         ──▄──██▌─▄──
+         ▄▀─█▀██▀█─▀▄
+         ▐▌▐▌─▐▌─▐▌▐▌
+         ─▐─█────█─▌─
+         ────▌──▐────
+'
+echo -e "Mr.R0Y4L SM0K3R "
